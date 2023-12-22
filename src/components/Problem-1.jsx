6187d0f3fcf2a09e38d5a82e1c246c25fea431a7
fowjaelahamed <fowjaelahamed@gmail.com>
2023-12-22 23:32:04 +0600

    probelm 1 sovled

--- src/components/Problem-1.jsx
+++ src/components/Problem-1.jsx
@@ -1,59 +1,119 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
 const Problem1 = () => {
+  const [show, setShow] = useState("all");
+  const [tasks, setTasks] = useState([]);
+  const [name, setName] = useState("");
+  const [status, setStatus] = useState("All");
 
-    const [show, setShow] = useState('all');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newTask = { name, status };
+    setTasks([...tasks, newTask]);
+    setName("");
+    setStatus("");
+  };
 
-    const handleClick = (val) =>{
-        setShow(val);
-    }
+  const sortTasks = () => {
+    return tasks.sort((a, b) => {
+      if (a.status === 'active' && b.status !== 'active') return -1;
+      if (a.status === 'completed' && b.status !== 'active') return 1;
+      return 0;
+    });
+  };
 
-    return (
+  const filterTasks = () => {
+    return (status === 'all' || status === "")  ? sortTasks() : sortTasks().filter((task) => task.status === status);
+  };
 
-        <div className="container">
-            <div className="row justify-content-center mt-5">
-                <h4 className='text-center text-uppercase mb-5'>Problem-1</h4>
-                <div className="col-6 ">
-                    <form className="row gy-2 gx-3 align-items-center mb-4">
-                        <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Name"/>
-                        </div>
-                        <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Status"/>
-                        </div>
-                        <div className="col-auto">
-                            <button type="submit" className="btn btn-primary">Submit</button>
-                        </div>
-                    </form>
-                </div>
-                <div className="col-8">
-                    <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
-                        <li className="nav-item">
-                            <button  className={`nav-link ${show==='all' && 'active'}`} type="button" onClick={()=>handleClick('all')}>All</button>
-                        </li>
-                        <li className="nav-item">
-                            <button className={`nav-link ${show==='active' && 'active'}`} type="button" onClick={()=>handleClick('active')}>Active</button>
-                        </li>
-                        <li className="nav-item">
-                            <button  className={`nav-link ${show==='completed' && 'active'}`} type="button" onClick={()=>handleClick('completed')}>Completed</button>
-                        </li>
-                    </ul>
-                    <div className="tab-content"></div>
-                    <table className="table table-striped ">
-                        <thead>
-                        <tr>
-                            <th scope="col">Name</th>
-                            <th scope="col">Status</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        
-                        </tbody>
-                    </table>
-                </div>
+  console.log(tasks);
+
+  return (
+    <div className="container">
+      <div className="row justify-content-center mt-5">
+        <h4 className="text-center text-uppercase mb-5">Problem-1</h4>
+        <div className="col-6 ">
+          <form className="row gy-2 gx-3 align-items-center mb-4">
+            <div className="col-auto">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
             </div>
+            <div className="col-auto">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Status"
+                value={status}
+                onChange={(e) => setStatus(e.target.value.toLowerCase())}
+              />
+            </div>
+            <div className="col-auto">
+              <button
+                type="submit"
+                className="btn btn-primary"
+                onClick={handleSubmit}
+              >
+                Submit
+              </button>
+            </div>
+          </form>
         </div>
-    );
+        <div className="col-8">
+          <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
+            <li className="nav-item">
+              <button
+                className={`nav-link ${(status === 'all' || status === "") && "active"}`}
+                type="button"
+                onClick={() => setStatus("all")}
+              >
+                All
+              </button>
+            </li>
+            <li className="nav-item">
+              <button
+                className={`nav-link ${status === "active" && "active"}`}
+                type="button"
+                onClick={() => setStatus("active")}
+              >
+                Active
+              </button>
+            </li>
+            <li className="nav-item">
+              <button
+                className={`nav-link ${status === "completed" && "active"}`}
+                type="button"
+                onClick={() => setStatus("completed")}
+              >
+                Completed
+              </button>
+            </li>
+          </ul>
+          <div className="tab-content"></div>
+          <table className="table table-striped ">
+            <thead>
+              <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              {filterTasks().map((task, index) => (
+                <tr key={index}>
+                  <td>{task.name}</td>
+                  <td>{task.status}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Problem1;
